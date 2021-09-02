9c684bd25191be674958c29a479b98afc08f3097
karrabl4st <karraisepic@gmail.com>
2021-09-02 01:37:19 -0500

    edited TreeFrame.java

--- src/TreeFrame.java
+++ src/TreeFrame.java
@@ -1,7 +1,20 @@
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class TreeFrame extends JFrame {
 	
-
+	static int offset = 0;
+	
+	@Override
+	public void paint(Graphics g) {
+		
+		if (offset < 100) {
+			if (offset == 0) {
+				g.drawLine(0, 0, offset, offset);
+			}
+		}
+		
+	}
 
 }
