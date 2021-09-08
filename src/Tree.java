10b11b289289b959931d2b8f4f6bb7fc47a11bb8
karrabl4st <karraisepic@gmail.com>
2021-09-07 19:35:36 -0500

    add more to be in line with format

--- src/Tree.java
+++ src/Tree.java
@@ -35,5 +35,42 @@ public class Tree {
 		return maxSegments;
 		
 	}
+	
+	public void drawOn(Graphics g, int x, int y, double angle) {
+		
+		
+		
+	}
+	
+	private void drawTree(Graphics g, int sx, int sy, double sAngle, int segsRemaining) {
+		
+		if (segsRemaining > 0) {
+			
+			if (segsRemaining < 4) {
+				
+				//0 is placeholder
+				drawLeaf(g, sx, sy, 0);
+				
+			}
+			
+			drawTree(g, sx, sy, sAngle, segsRemaining);
+			
+		}
+		
+	}
+	
+	private void drawLeaf(Graphics g, int sx, int sy, double theta) {
+		
+		
+		
+	}
+	
+	private int branchLength(int segsRemaining) {
+		
+		int branchLength = 0;
+		
+		return branchLength;
+		
+	}
 
 }
